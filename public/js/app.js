c64624b9fd3e5cdbb6c47ee227e999a0d0ae943d
Mason <maseh87@gmail.com>
2014-04-20 16:46:56 -0700

    modified views

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,6 @@ angular.module('recipe-app', [
 
 
   $scope.getFood = function(food) {
-
     return $http({
       method: 'GET',
       url: '/' + food,
@@ -25,26 +24,49 @@ angular.module('recipe-app', [
     then(function(data){
       $scope.foods = sortData(data.data);
     })
-    return food = '';
 
   };
 
   $scope.ingredient;
   $scope.allRecipes = [];
   var sortData = function(data) {
-    var matches = data.matches;
-    for(var i = 0; i < matches.length; i++) {
-      if(matches[i].smallImageUrls[0]) {
-        $scope.allRecipes.push({
-          name: matches[i].recipeName,
-          ingredients: matches[i].ingredients,
-          pic: matches[i].smallImageUrls[0]
-        });
+    if($scope.allRecipes.length > 0) {
+      $scope.removeCard(0);
+      $scope.allRecipes = [];
+      var matches = data.matches;
+      for(var i = 0; i < matches.length; i++) {
+        if(matches[i].smallImageUrls[0]) {
+          $scope.allRecipes.push({
+            name: matches[i].recipeName,
+            ingredients: matches[i].ingredients,
+            pic: matches[i].smallImageUrls[0]
+          });
+        }
+      }
+      // console.log($scope.allRecipes.ingredients);
+    } else if ($scope.allRecipes.length <= 0 || 0) {
+      var matches = data.matches;
+      for(var i = 0; i < matches.length; i++) {
+        if(matches[i].smallImageUrls[0]) {
+          $scope.allRecipes.push({
+            name: matches[i].recipeName,
+            ingredients: matches[i].ingredients,
+            pic: matches[i].smallImageUrls[0]
+          });
+        }
       }
     }
   };
-
+  $scope.onEnter = function(event, food) {
+    if(event.keyCode === 13) {
+      $scope.getFood(food);
+    }
+  };
   $scope.removeCard = function(index) {
+    console.log(index);
+    if(index === 0) {
+      $scope.allRecipes.splice(1);
+    }
     $scope.allRecipes.splice(0, index);
     $scope.allRecipes.splice(index);
   };
